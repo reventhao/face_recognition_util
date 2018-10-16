1bf876ee167a199fb2377ed82c8829f6b1515b85
reventhao <ren895873@gmail.com>
2018-10-16 15:43:57 +0800

    wsgi参数配置

--- util/face_recognition_server.py
+++ util/face_recognition_server.py
@@ -2,8 +2,13 @@ from flask import Flask, request
 from util.face_service import get_contrast_faces_result
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
+import configparser
 
 monkey.patch_all()
+config = configparser.ConfigParser()
+config.read("wsgi_config.conf")
+IP = config['config']['ip']
+PORT = int(config['config']['port'])
 app = Flask(__name__)
 
 
@@ -17,5 +22,5 @@ def contrast_faces_result():
 
 
 if __name__ == "__main__":
-    http_server = WSGIServer(('192.168.131.146', 8080), app)
+    http_server = WSGIServer((IP, PORT), app)
     http_server.serve_forever()
